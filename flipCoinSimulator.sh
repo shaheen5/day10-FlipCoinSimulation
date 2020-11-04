9fff00b034ef340eb24ef8f9710c30c3672f6a2e
unknown <miyashaheen1995@gmail.com>
2020-11-05 00:55:33 +0530

    Modify:functionalities

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,18 +3,27 @@
 echo "Flip Coin Simulation!!!"
 head_count=0
 tail_count=0
-count=0
-while [ $count -lt 10 ]
+while [ $head_count -lt 21 ] && [ $tail_count -lt 21 ]
 do
 	if [ $(( RANDOM%2 )) -eq 0 ]
 	then
-		echo "Heads!"
 		((head_count++))
 	else
-		echo "Tails!"
 		((tail_count++))
 	fi
-	((count++))
 done
 echo "No. of times HEAD has won="$head_count
 echo "No. of times TAIL has won="$tail_count
+if [ $head_count -eq $tail_count ]
+then
+	echo "It's a Tie!!"
+else
+	echo "Its a Win!!"
+fi
+
+if [ $head_count -eq 21 ]
+then
+	echo "HEADS WON BY $(($head_count - $tail_count)) POINTS"
+else
+	echo "TAILS WON BY $((tail_count - $head_count)) POINTS"
+fi
