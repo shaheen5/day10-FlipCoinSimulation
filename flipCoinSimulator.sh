8c928e8429880d4d19deaa2fa3160928256ceb82
unknown <miyashaheen1995@gmail.com>
2020-11-05 00:11:44 +0530

    Addd:functionalities

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
 
 echo "Flip Coin Simulation!!!"
-if [ $(( RANDOM%2 )) -eq 0 ]
-then
-	echo "Heads!"
-else
-	echo "Tails!"
-fi
+head_count=0
+tail_count=0
+count=0
+while [ $count -lt 10 ]
+do
+	if [ $(( RANDOM%2 )) -eq 0 ]
+	then
+		echo "Heads!"
+		((head_count++))
+	else
+		echo "Tails!"
+		((tail_count++))
+	fi
+	((count++))
+done
+echo "No. of times HEAD has won="$head_count
+echo "No. of times TAIL has won="$tail_count
